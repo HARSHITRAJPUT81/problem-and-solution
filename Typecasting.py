513713a77558c483b3536d419a811f861872a61a
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-09 22:44:10 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,26 @@
+# str="15"
+# num=7
+# str_num=int(str)
+# sum=num+str_num
+# print("The sum of the both numbers is:",sum)
+
+
+# #Python automatically convert
+# #a to int 
+# a=7
+# print(type(a))
+# #python automatically convert b to float
+# b=7.0
+# print(type(b))
+# #python automatically convert c to float as it is a float addition  
+# c=a+b
+# print(c)
+# print(type(c))
+
+# a=input("Enter your name : ")
+# print("My name is", a)
+
+#quick quiz
+
+nm= "Harry"
+print(nm[-4:-2])
