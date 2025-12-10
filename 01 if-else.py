1382379a2d036a490527c607fd14d98202d2f1b2
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-10 22:24:45 +0530

    Add files via upload

---
+++ 01 if-else.py
@@ -0,0 +1,32 @@
+# a=int(input("Enter your age: "))
+# print("your age is:", a)
+# if (a>18):
+#     print("you can drive")
+
+#conditional operators
+# >, <, >=, <=, ==, !=  
+
+# print(a>18)
+# print(a<18)
+# print(a>=18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+
+
+# else:
+#     print("you can not drive")
+
+
+
+appleprice = 230
+budget = 210
+if (appleprice>budget):
+    (budget-appleprice > 50)
+    print("Alexa add 1kg apple into the cart.")
+
+elif (appleprice - budget > 70):
+    print("its okey you can buy.") 
+    
+else:
+    print("Alexa do not add the apple into the cart.")
