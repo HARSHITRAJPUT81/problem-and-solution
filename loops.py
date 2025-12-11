61d8e87a31cf92797ab9c6422f352fb264c8da23
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-11 22:58:12 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,74 @@
+# for Loop in Python 
+# Iterating over a string
+
+# name = "Abhishek"
+# for i in name:
+#     print(i)
+
+#     if i=="b":
+#         print("This is special word")
+
+
+
+#Iterating over a list
+
+# fruits = ["Mango", "Apple", "Banana", "Grapes"]
+# for fruits in fruits:
+#     print(fruits)
+
+#     for i in fruits:
+#         print(i)
+   
+# for Loop with range()
+# for k in range(5):
+#     print(k+1)
+
+# for k in range (1,20001):
+#     print(k)    
+
+
+
+#Third Parameter in range() function is step size
+# for k in range (12,0,-3):
+#     print(k)
+
+
+
+
+
+
+#While Loop in Python   
+
+# count=5
+# while(count>0):
+#     print(count)
+#     count=count-1
+
+
+# i=0
+# while(i<=3):
+#   print(i)
+#   i=i+1
+
+# print("Done with the loop")
+
+
+# i=int(input("Enter the number:"))
+# while(i<=38):
+#     i=int(input("Enter the number:"))
+#     print(i)
+
+# print("Done with the loop")    
+
+
+
+
+#Else with While loop
+
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+
+else:
+    print('Counter is 0')
