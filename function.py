16f4f0e10edc20338fcb9aabb200fd2fe721f4fd
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-12 22:20:53 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,35 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+
+def isGreater (a,b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater or equal")
+
+def isLesser (a,b):
+    pass
+
+
+a=9
+b=8
+
+isGreater(a,b)
+calculateGmean(a,b)
+
+#Gmean1 = (a*b)/(a+b)
+#print(Gmean1)
+
+c=8
+d=74
+
+isGreater(c,d)
+calculateGmean(c,d)
+
+#Gmean2 = (c*d)/(c+d)
+#print(Gmean2)
+
+  
+
