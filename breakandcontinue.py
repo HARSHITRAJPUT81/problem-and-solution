aa2567127cc841a2fd86c0381c7ba692b6aae44b
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-14 21:28:16 +0530

    Add files via upload

---
+++ breakandcontinue.py
@@ -0,0 +1,27 @@
+# Break Statement 
+
+# for i in range(1,101,1):
+#     print(i, end="")
+#     if(i==50):
+#         break
+#     else:
+#         print("mississippi")
+
+# print("Thank You")        
+
+
+
+
+# Continue Statement
+
+# for i in range(12):
+#     if(i==10):
+#         print("Skip the iteration")
+#         continue
+#     print("5 X",i+1,"=",5*(i+1))
+
+
+for i in [2,3,4,6,8,0]:    
+    if(i%2!=0):
+        continue
+    print(i)
