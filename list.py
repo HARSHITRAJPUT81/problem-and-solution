f12218026a5c78ffae83e058c6b717ed4aeada69
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-15 22:36:44 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+l = [3,5,8]
+# print(marks)
+print(type(l))
+print(l)
+
+marks = [3,5,7]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[1])
+
+
+
+colours = ["Red","Green","Blue","Yellow","Green"]
+if "Yellow" in colours:
+    print("Yes")
+else:
+    print("No")    
+
+
+details = ["Abhijeet",18,"FYBScIT",9.8]
+print(details)
