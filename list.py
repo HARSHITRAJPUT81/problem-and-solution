810138a2e28e30e3740b031176b725be2dca2b72
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-16 22:22:26 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -20,3 +20,29 @@ else:
 
 details = ["Abhijeet",18,"FYBScIT",9.8]
 print(details)
+
+
+
+#Negative Indexing
+marks = [3,5,6,"Harry",True ,6,4,0,3,2,6,8]
+print(marks[-3]) #Negative index
+print(len(marks)-3) #Positive index 
+print(marks[5-3]) #Positive index
+print(marks[2]) #Positive index 
+
+if 6 in  marks:
+    print("yes")
+else:
+    print("no")    
+
+if"arry" in "Harry":
+    print("yes")  
+
+
+#Jump indexing  
+marks = [3,5,6,"Harry",True ,6,4,0,3,2,6,8]
+print(marks[0:7])
+print(marks[1:9])
+print(marks[1:9:2])
+
+
