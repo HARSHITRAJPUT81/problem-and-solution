2f8756184797d77c6fdd69840872aefb95ac53a2
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-19 23:50:28 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,17 @@
+tup = (1,3,4,8,5,True,"xyz",9)
+print(type(tup) )
+print(tup)
+print(tup[0])
+print(tup[1])
+print(tup[2])
+print(tup[-1])
+
+if 8 in tup:
+    print("it is present ") 
+
+tup2 = tup[1:4]
+print(tup2)
+
+
+print(tup[1:4])
+
