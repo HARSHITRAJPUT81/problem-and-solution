0c47b2115c2404130b61fa389d2a06c8396b96d7
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-26 23:41:38 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,17 @@
+dic = {
+    "harry":"human being",
+    "spoon":"object"
+}
+print(dic["harry"])
+print(dic["spoon"])
+
+
+student = {
+    "name": "Ashwani Kumar",
+    "age": 21,
+    "course": "Generative AI",
+    "skills": ["Python", "Machine Learning", "Ethical Hacking"]
+}
+print(student["name"])
+print(student["skills"])
+print(student["course"])
