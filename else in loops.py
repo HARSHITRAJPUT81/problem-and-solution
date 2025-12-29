f53149af234404dfe0ef17e5acc95c38ace958f7
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2025-12-29 22:41:21 +0530

    Add files via upload

---
+++ else in loops.py
@@ -0,0 +1,27 @@
+#For-else 
+#it is uniquefeature in python.
+for i in range(6):
+    print(i)
+    if i == 4:
+        break
+else:
+    print("Loop poori tarah complete ho gaya")
+
+
+#While-else
+i = 1
+
+while i <= 3:
+    print(i)
+    i += 1
+else:
+    print("While loop khatam ho gaya")
+
+
+#Summary
+
+#Use case                       When else run    
+                                 
+#For-else                        Jab loop'break'ke bina complete ho
+
+#While-else                     Jab loop normally end ho
