e3396b9606d3415f1f357aef511b60a19c77dda3
Harshit Rajput <154085497+HARSHITRAJPUT81@users.noreply.github.com>
2026-01-08 23:58:46 +0530

    Add files via upload

---
+++ if "== name==main".py
@@ -0,0 +1,19 @@
+# def greet(name):
+#     print("Hello,", name)
+
+# # main block
+# if __name__ == "__main__":
+#     greet("Harshit")
+
+
+# def add(a, b):
+#     return a + b
+
+# if __name__ == "__main__":
+#     print(add(2, 3))
+
+def main():
+    #code to be run when the script run directly
+    print("Running script directly")
+if__name__=="__main__":
+main()    
